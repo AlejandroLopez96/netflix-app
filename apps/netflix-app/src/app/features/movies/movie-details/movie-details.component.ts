f5ddaedccd9ddd4efd673a08f6e62b74e2460c95
Alex <alexlopezortiz9@gmail.com>
2025-02-21 12:28:33 +0100

    initialize netflix-app with Angular

---
+++ apps/netflix-app/src/app/features/movies/movie-details/movie-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details',
+  imports: [],
+  templateUrl: './movie-details.component.html',
+  styleUrl: './movie-details.component.css'
+})
+export class MovieDetailsComponent {
+  movieId = input.required<string>();
+}
